6c7391de691507b140ac7d484f5db38a7e214fde
Rita Moura <ritamoura520@gmail.com>
2022-11-16 10:49:24 -0300

    Feat: Adiciona função generatePhoneNumber
    Essa função recebe um array como parâmetro e retorna um número de telefone dentro desse padrão (xx) xxxxx-xxxx, e verifica se o array recebido está dentro das condições necessárias para se tornar um número de telefone.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,16 +1,22 @@
 // Desafio 11
 function generatePhoneNumber(array) {
-  const ddd = `${array[0]}${array[1]}`;
-  const initNumber = `${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}`;
-  const findNumber = `${array[7]}${array[8]}${array[9]}${array[10]}`;
-  if (array.length !== 11) return 'Array com tamanho incorreto.';
-  if (array.map((e) => e < 0)) {
+  const verifyLength = array.length < 11 || array.length > 11;
+  const varifyNumbers = array.some((number) => number < 0 || number > 9);
+  const repeat = [];
+  array.forEach((e) => {
+    repeat[e] = (repeat[e] || 0) + 1;
+  });
+  const verify = repeat.some((number) => number >= 3);
+  if (verifyLength) return 'Array com tamanho incorreto.';
+  if (varifyNumbers || verify) {
     return 'não é possível gerar um número de telefone com esses valores';
   }
-  return `(${ddd}) ${initNumber}-${findNumber}`;
+  let numberPhone = '(xx) xxxxx-xxxx';
+  array.forEach((item) => {
+    numberPhone = numberPhone.replace('x', item);
+  });
+  return numberPhone;
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
-// '(12) 34567-8901'
 
 // Desafio 12
 function triangleCheck() {
